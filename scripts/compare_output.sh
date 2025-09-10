0ba3bee5cb5034543fbb85502132ffbc8a283fac
Tam Nguyen <68078554+ball2004244@users.noreply.github.com>
2025-09-10 07:13:14 +0000

    test(output): verify output are same in terminal as in log file and sample output

--- scripts/compare_output.sh
+++ scripts/compare_output.sh
@@ -5,25 +5,27 @@
 # To compare against provided sample output, modify io/InCollege_Input.txt and io/InCollege_SampleOutput.txt to test different scenarios.
 
 cd ..
-# Compile the program if it is not compiled yet
-if [ ! -f "./incollege" ]; then
-    cd src
-    echo "Unable to find the compiled program. Compiling now..."
-    cobc -x InCollege.cob -o ../incollege
-    cd ..
+# Force compile the program
+if [ -f "./incollege" ]; then
+    rm incollege
 fi
+cd src
+echo "Force compiling the program to ensure latest changes..."
+cobc -x InCollege.cob -o ../incollege
+cd ..
+
 
 # Run the program and redirect output to a temporary file
 mkdir -p temp
 ./incollege > temp/output.txt 2>&1
 
 # Compare the output with the expected output
-if diff -q temp/output.txt io/InCollege_Output.txt > /dev/null; then
+if diff -q temp/output.txt io/InCollege-Output.txt > /dev/null; then
     echo "- Terminal output matches file output."
 else
     echo "- Terminal output does not match file output."
     echo "Differences:"
-    diff temp/output.txt io/InCollege_Output.txt
+    diff temp/output.txt io/InCollege-Output.txt
 
     exit 1
 fi
